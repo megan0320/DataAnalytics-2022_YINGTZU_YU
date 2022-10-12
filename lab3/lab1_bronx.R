aead9c63b31781e8c27bfc0bcf332fb38ecd6aad
megan0320 <45302788+megan0320@users.noreply.github.com>
2022-10-12 00:28:45 -0400

    Add files via upload

--- lab3/lab1_bronx.R
+++ lab3/lab1_bronx.R
@@ -1,49 +1,42 @@
 library(gdata) 
-#alternate
-bronx1<-read.xls("C:\\Apps\\rollingsales_bronx.xls",pattern="BOROUGH",verbose=FALSE, stringsAsFactors=FALSE,sheet=1,header=TRUE,perl="C:\\Strawberry\\perl\\bin\\perl.exe")
+#faster xls reader but requires perl!
+bronx1<-read.xls("C:\\Apps\\rollingsales_bronx.xls",sheet=1,verbose=FALSE,pattern="BOROUGH",stringsAsFactors=FALSE,perl="C:\\Strawberry\\perl\\bin\\perl.exe") 
 bronx1<-bronx1[which(bronx1$GROSS.SQUARE.FEET!="0" & bronx1$LAND.SQUARE.FEET!="0" & bronx1$SALE.PRICE!="$0"),]
-View(bronx1)
-#
 attach(bronx1) # If you choose to attach, leave out the "data=." in lm regression
 
+help(gsub)
+SALE.PRICE<-(gsub(",","", bronx1$SALE.PRICE)) 
+SALE.PRICE<-sub("\\$","",SALE.PRICE)
 SALE.PRICE
-SALE.PRICE<-gsub(",","", SALE.PRICE)
-SALE.PRICE
-SALE.PRICE<-sub("\\$","",SALE.PRICE) 
 SALE.PRICE<-as.numeric(SALE.PRICE)
 
-#SALE.PRICE <- na.omit(SALE.PRICE) # delete rows with NA
-GROSS.SQUARE.FEET<-as.numeric(gsub(",","", GROSS.SQUARE.FEET)) 
-LAND.SQUARE.FEET<-as.numeric(gsub(",","", LAND.SQUARE.FEET)) 
-LAND.SQUARE.FEET
+GROSS.SQUARE.FEET<-as.numeric(gsub(",","", bronx1$GROSS.SQUARE.FEET)) 
+LAND.SQUARE.FEET<-as.numeric(gsub(",","", bronx1$LAND.SQUARE.FEET)) 
+
 plot(log(GROSS.SQUARE.FEET), log(SALE.PRICE)) 
-
-GROSS.SQUARE.FEET
 m1<-lm(log(SALE.PRICE)~log(GROSS.SQUARE.FEET))
-
 summary(m1)
 abline(m1,col="red",lwd=2)
 plot(resid(m1))
 
 # Model 2
-#bronx1$LAND.SQUARE.FEET <-as.numeric(bronx1$LAND.SQUARE.FEET)
-LAND.SQUARE.FEET
-bronx1$NEIGHBORHOOD
-m2<-lm(log(SALE.PRICE)~log(GROSS.SQUARE.FEET)+log(LAND.SQUARE.FEET)+factor(NEIGHBORHOOD))
+
+m2<-lm(log(bronx1$SALE.PRICE)~log(bronx1$GROSS.SQUARE.FEET)+log(bronx1$LAND.SQUARE.FEET)+factor(bronx1$NEIGHBORHOOD))
 summary(m2)
 plot(resid(m2))
+
 # Suppress intercept - using "0+ ..."
-m2a<-lm(log(SALE.PRICE)~0+log(GROSS.SQUARE.FEET)+log(LAND.SQUARE.FEET)+factor(NEIGHBORHOOD))
+m2a<-lm(log(bronx1$SALE.PRICE)~0+log(bronx1$GROSS.SQUARE.FEET)+log(bronx1$LAND.SQUARE.FEET)+factor(bronx1$NEIGHBORHOOD))
 summary(m2a)
 plot(resid(m2a))
 
 # Model 3
-m3<-lm(log(SALE.PRICE)~0+log(GROSS.SQUARE.FEET)+log(LAND.SQUARE.FEET)+factor(NEIGHBORHOOD)+factor(bronx1$BUILDING.CLASS.CATEGORY))
+m3<-lm(log(bronx1$SALE.PRICE)~0+log(bronx1$GROSS.SQUARE.FEET)+log(bronx1$LAND.SQUARE.FEET)+factor(bronx1$NEIGHBORHOOD)+factor(bronx1$BUILDING.CLASS.CATEGORY))
 summary(m3)
 plot(resid(m3))
 
 # Model 4
-m4<-lm(log(SALE.PRICE)~0+log(GROSS.SQUARE.FEET)+log(LAND.SQUARE.FEET)+factor(NEIGHBORHOOD)*factor(BUILDING.CLASS.CATEGORY))
+m4<-lm(log(bronx1$SALE.PRICE)~0+log(bronx1$GROSS.SQUARE.FEET)+log(bronx1$LAND.SQUARE.FEET)+factor(bronx1$NEIGHBORHOOD)*factor(bronx1$BUILDING.CLASS.CATEGORY))
 summary(m4)
 plot(resid(m4))
-#
+
